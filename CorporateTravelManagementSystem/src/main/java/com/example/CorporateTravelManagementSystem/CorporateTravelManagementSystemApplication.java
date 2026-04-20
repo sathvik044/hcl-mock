5f687b002fbef41ce4c69bf0bc243ff56f8c1ad2
sathvik044 <sathvikvadapalli123@gmail.com>
2026-04-20 10:57:39 +0530

    first commit

---
+++ CorporateTravelManagementSystem/src/main/java/com/example/CorporateTravelManagementSystem/CorporateTravelManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package com.example.CorporateTravelManagementSystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CorporateTravelManagementSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CorporateTravelManagementSystemApplication.class, args);
+	}
+
+}
---
+++ CorporateTravelManagementSystem/src/test/java/com/example/CorporateTravelManagementSystem/CorporateTravelManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.CorporateTravelManagementSystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CorporateTravelManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
